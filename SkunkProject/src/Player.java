0fc2a204cb120f7fb93d742d206c156a087cd69c
davidslack-seis635 <slac5601@stthomas.edu>
2018-10-21 22:05:19 -0500

    Added Round and Player classes.

---
+++ SkunkProject/src/Player.java
@@ -0,0 +1,11 @@
+
+public class Player {
+
+	private String name;
+	private int chips = 50;
+	private int score = 0;
+	
+	public Player(String name) {
+		this.name = name;
+	}
+}
--- SkunkProject/src/Turn.java
+++ SkunkProject/src/Turn.java
@@ -1,4 +1,12 @@
 
 public class Turn {
+	
+	private int startingScore;
+	private int turnScore = 0;
+	
+	
+	public Turn(int startingTurnScore) {
+		this.startingScore = startingTurnScore;
+	}
 
 }
